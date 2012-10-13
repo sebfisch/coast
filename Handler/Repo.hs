f50d81a7316be1db6d1692144934691084225965
Sebastian Fischer <mail@sebfisch.de>
2012-10-13 22:19:22 +0200

    added directory listing

--- Handler/Repo.hs
+++ Handler/Repo.hs
@@ -1,21 +1,45 @@
 module Handler.Repo where
 
 import Import
-import System.Directory (doesDirectoryExist,doesFileExist)
-import Data.List (intercalate)
+import System.Directory
+import Data.List
+import Data.Char
+import Data.Function
+
+reposPath :: FilePath
+reposPath = "var/repos"
+
+makePath :: [FilePath] -> FilePath
+makePath path = intercalate "/" path
 
 getRepoR :: [String] -> Handler RepHtml
 getRepoR []             = redirect HomeR
 getRepoR names@(name:_) = do
     let links = [(name, RepoR [name])]
         top_navigation = $(widgetFile "top-navigation")
-        path = intercalate "/" ("var":"repos":names)
+        path = makePath names
+        fullPath = makePath (reposPath:names)
 
-    isDir   <- liftIO $ doesDirectoryExist path
-    isFile  <- liftIO $ doesFileExist path
+    isDir   <- liftIO $ doesDirectoryExist fullPath
+    isFile  <- liftIO $ doesFileExist fullPath
 
     if isDir then do
-        defaultLayout $(widgetFile "homepage")
+        contents <- liftIO $
+                        (   fmap (sortOn (map toLower)) $
+                            fmap (filter ((/='.') . head)) $
+                            fmap (filter (/="_darcs")) $
+                            getDirectoryContents fullPath
+                        ) >>= mapM (markDirectory fullPath)
+        let makeRoute file = RepoR $ names ++ [file]
+        defaultLayout $(widgetFile "repos-dir")
       else if isFile then do
         defaultLayout $(widgetFile "homepage")
       else notFound
+
+markDirectory :: FilePath -> FilePath -> IO (Bool,FilePath)
+markDirectory prefix file = do 
+    isDir <- liftIO $ doesDirectoryExist $ makePath [prefix,file]
+    return (isDir,file)
+
+sortOn :: Ord b => (a -> b) -> [a] -> [a]
+sortOn f = sortBy (compare `on` f)
